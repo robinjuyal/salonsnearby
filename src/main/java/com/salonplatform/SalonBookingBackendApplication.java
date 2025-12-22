4149110a08c87fc975b60d2a2e561ed67180eab4
robinjuyal <85496825+robinjuyal@users.noreply.github.com>
2025-12-22 16:41:55 +0530

    initial commit

---
+++ src/main/java/com/salonplatform/SalonBookingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.salonplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonBookingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalonBookingBackendApplication.class, args);
+	}
+
+}
